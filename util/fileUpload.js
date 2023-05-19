44511b26710c8da13bb738181e4e3a797c53a2cb
hager3012 <hagershaaban7@gmail.com>
2023-05-19 13:48:38 +0300

    modified:   util/fileUpload.js

--- util/fileUpload.js
+++ util/fileUpload.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/')
+        cb(null, './')
     },
     filename: function (req, file, cb) {
         cb(null,uuidv4()+"-"+ file.originalname)
